d1f8d434ccb9c5057cf429aac5cab367e9d77653
SergeyKalutsky <skalutsky@gmail.com>
2022-02-12 16:09:41 +0300

    refactored a bit

--- main.py
+++ main.py
@@ -14,15 +14,15 @@ def draw():
 
 def update(dt):
     if keyboard.left and peco.x > 20:
-        peco.x = peco.x - 5
+        peco.x -= 5
         peco.image  = 'left'
     elif keyboard.right and peco.x < 580:
-        peco.x = peco.x + 5
+        peco.x += 5
         peco.image = "right"
     elif keyboard.up:
-        peco.y = peco.y - 5
+        peco.y -= 5
         peco.image = "back"
     elif keyboard.down:
-        peco.y = peco.y + 5
+        peco.y += 5
         peco.image = "front"
 pgzrun.go()
