66c23734589625bdcf743e6550b0e396a4e6bd39
SergeyKalutsky <skalutsky@gmail.com>
2022-02-26 14:02:14 +0300

    chest resize

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ btn = Actor('start_btn', (300, 300))
 jellotrap = Actor("jelo_trap")
 zombie = Actor('zombie_front', (200, 300))
 chest = Actor("chest", (150, 100), (40, 40))
-
 game_mode = 'menu'
+
 
 def draw():
     if game_mode == 'menu':
@@ -47,29 +47,21 @@ def player_movement():
 
 
 def update_enemy(enemy):
-    if enemy.colliderect(chest):
-        if chest.top < zombie.bottom and zombie.y < chest.y:
-            zombie.bottom = chest.top - 0.5
-        elif chest.bottom < zombie.top and zombie.y > chest.y:
-            zombie.top = chest.bottom + 0.5
-        elif chest.right > zombie.left and zombie.x > chest.x:
-            zombie.left = chest.right + 0.5
-        elif chest.left < zombie.right and zombie.x < chest.x:
-            zombie.right = chest.left - 0.5
+    zombie.prev_pos = zombie.pos
 
-    elif zombie.x - 50 < peco.x < zombie.x + 50 and \
-            zombie.y - 100 < peco.y < zombie.y + 100:
+    if zombie.x - 100 < peco.x < zombie.x + 100 and \
+            zombie.y - 150 < peco.y < zombie.y + 150:
         if zombie.x < peco.x:
-            zombie.x += 1
+            zombie.x += 0.5
             zombie.image = 'zombie_right'
         else:
-            zombie.x -= 1
+            zombie.x -= 0.5
             zombie.image = 'zombie_left'
         if zombie.y < peco.y:
-            zombie.y += 1
+            zombie.y += 0.5
             zombie.image = 'zombie_front'
         else:
-            zombie.y -= 1
+            zombie.y -= 0.5
             zombie.image = 'zombie_back'
     else:
         zombie.x -= 0.5
@@ -78,12 +70,17 @@ def update_enemy(enemy):
             zombie.x = WIDTH + 5
             zombie.y = randint(10, HEIGHT - 10)
 
+    if enemy.colliderect(chest):
+        zombie.pos = zombie.prev_pos
+
     return zombie
+
 
 def on_mouse_down(pos):
     global game_mode
     if game_mode == 'menu' and btn.collidepoint(pos):
         game_mode = 'game'
+
 
 def update(dt):
     global zombie
